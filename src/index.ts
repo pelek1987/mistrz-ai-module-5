698cd861baedbd132efe6b4fa5f9b2d11a75498c
Pawel Sioda <pawel.sioda@gmail.com>
2024-01-25 00:29:18 +0100

    add openai lib to project

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World');
+import OpenAI from 'openai';
+
+const openai = new OpenAI({
+    apiKey: process.env['OPENAI_API_KEY']
+});
+
+const chatCompletion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'Say this is a test' }],
+    model: 'gpt-3.5-turbo',
+});
+
+console.log(chatCompletion);
+
